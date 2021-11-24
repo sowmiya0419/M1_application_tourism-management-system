9323fdbb615c82f2d1e7db3b6d69e6cedf277a1f
sathish20pandian <94277094+sathish20pandian@users.noreply.github.com>
2021-11-24 18:23:07 +0530

    Delete checking ticket.c

--- 3_Implentation/src/checking ticket.c
+++
@@ -1,3 +0,0 @@
-void checking_ticket(userprofile <em>hello) { while(hello!=NULL) { if(!strcmp(hello-&gt;user_fname,current_client)) break; hello=hello-&gt;next; } if(!strcmp(hello-&gt;user_place,"\0") || hello-&gt;ticket_price==0.0 || hello-&gt;user_numtick==0) { printf("You do not have a ticket booked yet\n"); return; } float tot=0.0; tot=(hello-&gt;ticket_price)</em>(hello-&gt;user_numtick);
-printf("You have booked %d tickets for a sum total of P %f for tour code %s\n",hello-&gt;user_numtick,tot,hello-&gt;user_place);
-}
