784c44f3a8ae3a3028df55e8ed69ef8beb5b8a8a
sathish20pandian <94277094+sathish20pandian@users.noreply.github.com>
2021-11-24 18:22:42 +0530

    Delete brochure.c

--- 3_Implentation/src/brochure.c
+++
@@ -1,6 +0,0 @@
-void ShowBrochure()
-{
-system("CLS");
-printf("\tPRICE LIST\n=============================\n1. AA - Isla Gigantis Tours - P 3000\n2. BB - Mag-Aso Falls Tours - P 5000\n3. CC - Mambukal Resort Tours - P 2000\n4. DD - Boracay Beach Resort - P 10000\n"
-"5. EE - Whispering Palms Island Resort - P 12000\n6. FF - La Vista Highlands - P 5000\n7. GG - Punta Bulata Resort &amp; Spa - P 7000\n8. HH - Big BamBoo Beach Resort - P 8000\n9. II - Nature's Village Resort - P 12000\n10. JJ - Artistic Diving &amp; Beach Resort - P 14000\n");
-}
